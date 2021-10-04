b67cecca819aa6ed0e91dafce7de5904f975d16c
Angelo Huang <huangelo02@gmail.com>
2021-10-04 11:32:34 +0200

    finish: project 04-00

--- 04-00/fill/Fill.asm
+++ 04-00/fill/Fill.asm
@@ -1,3 +1,73 @@
+(INPUT)
+	// i = SCREEN MEMORY, PER IL LOOP
+	@SCREEN
+	D=A 
+	@i
+	M=D 
+
+	// D=INPUT TASTIERA
+	@KBD 
+	D=M	
+
+	// SE D=0 TORNA A PRENDERE INPUT
+	@LOOPWHITE
+	D;JEQ
+
+	// ALTRIMENTI VADO A LOOP BLACK
+	@LOOPBLACK
+	D;JNE
+
+(LOOPWHITE)
+	// TRASFORMA IL BIT IN BIANCO
+	@i
+	A=M
+	M=0
+	
+	// I = I + 1
+	@i
+	M=M+1
+
+	// D = I - KBD UTILE PER COMPARARE
+	// SE D=0 SIGNIFICA CHE I = KBD, QUINDI FINITO
+	@i 
+	D=M
+	@KBD 
+	D=D-A 
+
+	// SE D!=0 NON HO FINITO QUINDI CONTINUA
+	@LOOPWHITE
+	D;JNE
+
+	// ALTRIMENTI TORNA ALL'INIZIO
+	@INPUT
+	D;JEQ
+
+(LOOPBLACK)
+	// TRASFORMA IL BIT IN NERO
+	@i
+	A=M
+	M=-1
+
+	// I = I + 1
+	@i
+	M=M+1
+
+	// D = I - KBD UTILE PER COMPARARE
+	// SE D=0 SIGNIFICA CHE I = KBD, QUINDI FINITO
+	@i 
+	D=M
+	@KBD 
+	D=D-A 
+
+	// SE D!=0 NON HO FINITO QUINDI CONTINUA
+	@LOOPBLACK
+	D;JNE
+
+(END)
+	@INPUT
+	0;JMP
+
+
 // This file is part of www.nand2tetris.org
 // and the book "The Elements of Computing Systems"
 // by Nisan and Schocken, MIT Press.
@@ -10,5 +80,3 @@
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
-
-// Put your code here.
--- 04-00/mult/Mult.asm
+++ 04-00/mult/Mult.asm
@@ -1,3 +1,60 @@
+(BEGIN)
+	// if R0 < 0 then exit
+	@R0
+	D=M 
+	@END
+	D;JLT
+
+	// if R1 < 0 then exit
+	@R1
+	D=M 
+	@END
+	D;JLT
+
+(SETPARAMETERS)
+	// for(int i = 0; i < R1; i++);
+	D=0
+	@i 
+	M=D // I = 0
+
+	@R2
+	M=D // R2 = 0
+	
+(MULTIPLY)
+	// if i == R1 exit
+	@R1 
+	D=M 
+	@i 
+	D=D-M 
+	@END 
+	D;JEQ
+
+
+	// R2 = R0 + R2
+	@R0
+	D=M 
+	@R2
+	M=M+D 
+
+	// I += 1
+	@i 
+	M=M+1 
+
+	@MULTIPLY
+	0;JMP 
+
+(STORE)
+	@R2
+	D=M 
+	@R2 
+	M=D
+	@END
+	0;JMP
+
+(END)
+	@END
+	0;JMP 
+
 // This file is part of www.nand2tetris.org
 // and the book "The Elements of Computing Systems"
 // by Nisan and Schocken, MIT Press.
@@ -7,6 +64,4 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 //
 // This program only needs to handle arguments that satisfy
-// R0 >= 0, R1 >= 0, and R0*R1 < 32768.
-
-// Put your code here.
+// R0 >= 0, R1 >= 0, and R0*R1 < 32768
